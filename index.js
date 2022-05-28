f00ac83d29fd4803a33498bb0b8608f831b72d6d
Waheedul Islam <waheeduli81@gmail.com>
2022-05-28 23:04:43 +0600

    push

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ const port = process.env.PORT || 5000;
 
 
 //meddleWare
+// app.use(express.json())
+const corsConfig = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("*", cors(corsConfig))
 app.use(express.json())
-app.use(cors())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
+    next()
+})
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
